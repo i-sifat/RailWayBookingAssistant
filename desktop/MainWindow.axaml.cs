81d6ece043b5d97b8415d7a57bc2ed4deeaa4d52
i-sifat <isifat011@gmail.com>
2026-09-22 11:47:15 +0600

    desktop: add missing Avalonia.Controls.Primitives using for ToggleButton

--- desktop/MainWindow.axaml.cs
+++ desktop/MainWindow.axaml.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
 using Avalonia.Interactivity;
 using Avalonia.Media;
 using Avalonia.Styling;
